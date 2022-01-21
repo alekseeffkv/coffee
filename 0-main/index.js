ed794d7a60e4e026bcc3c58ad1442d937da5d39a
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-01-21 15:54:46 +0300

    added a new component 'Modal'

--- 0-main/index.js
+++ 0-main/index.js
@@ -4,6 +4,7 @@ import navItems from '../1-navigation/nav-items.js';
 import CartIcon from '../2-cart-icon/index.js';
 
 import Carousel from '../4-carousel/index.js';
+import Modal from '../5-modal/modal.js';
 
 export default class Main {
 
@@ -11,17 +12,17 @@ export default class Main {
   }
 
   async render() {
-    let navigation = new Navigation(navItems);
+    const navigation = new Navigation(navItems);
     document.querySelector('.hero__nav').append(navigation.elem);
 
-    let cartIcon = new CartIcon();
+    const cartIcon = new CartIcon();
     document.querySelector('.hero__cart-icon').append(cartIcon.elem);
 
-    let response = await fetch('products.json');
-    let json = await response.json();
-    let products = JSON.parse(JSON.stringify(json));
+    const response = await fetch('products.json');
+    const json = await response.json();
+    const products = JSON.parse(JSON.stringify(json));
 
-    let carousel = new Carousel(products);
+    const carousel = new Carousel(products);
     document.querySelector('[data-carousel-holder]').append(carousel.elem);
   }
 }
