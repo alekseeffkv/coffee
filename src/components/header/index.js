5b9f017febc7edc70dcd16d51a9fce849a674174
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-02-08 14:53:49 +0300

    add fixed header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,9 +4,29 @@ import './index.css';
 
 export default class Header {
   elem = null;
+  #header = null;
+  #initialTopCoord = null;
 
   constructor() {
     this.elem = this.#createHeader();
+    this.#header = this.elem.querySelector('.hero__header');
+    this.addEventListeners();
+  }
+
+  updatePosition() {
+    if (!this.#initialTopCoord) {
+    this.#initialTopCoord = this.#header.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    if (window.pageYOffset > this.#initialTopCoord) {
+      this.#header.classList.add('hero__header_fixed');
+    } else {
+      this.#header.classList.remove('hero__header_fixed');
+    }
+  }
+
+  addEventListeners() {
+    document.addEventListener('scroll', () => this.updatePosition());
   }
 
   #createHeader() {
@@ -15,12 +35,14 @@ export default class Header {
       <div class="hero__inner container">
 
         <div class="hero__header">
-          <div class="hero__logo">
-            ${logo}
-            <div class="hero__logo-text">Coffee</div>
+          <div class="hero__header-inner">
+            <div class="hero__logo">
+              ${logo}
+              <div class="hero__logo-text">Coffee</div>
+            </div>
+            <div class="hero__nav"></div>
+            <div class="hero__cart-icon"></div>
           </div>
-          <div class="hero__nav"></div>
-          <div class="hero__cart-icon"></div>
         </div>
 
         <div class="hero__text">
